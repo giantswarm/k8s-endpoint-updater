9c8a53a5bac5252504d10f825cf35dbcaa265bd7
Tim Schindler <xh3b4sd@gmail.com>
2017-03-06 16:47:49 +0100

    added updater service

--- command/command.go
+++ command/command.go
@@ -2,13 +2,18 @@
 package command
 
 import (
+	"net/url"
+
 	"github.com/spf13/cobra"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 
 	microerror "github.com/giantswarm/microkit/error"
 	micrologger "github.com/giantswarm/microkit/logger"
 
 	"github.com/giantswarm/k8s-endpoint-updater/command/update"
 	"github.com/giantswarm/k8s-endpoint-updater/command/version"
+	"github.com/giantswarm/k8s-endpoint-updater/service/updater"
 )
 
 // Config represents the configuration used to create a new root command.
@@ -17,6 +22,12 @@ type Config struct {
 	Logger micrologger.Logger
 
 	// Settings.
+	KubernetesAddress    string
+	KubernetesInCluster  bool
+	KubernetesTLSCaFile  string
+	KubernetesTLSCrtFile string
+	KubernetesTLSKeyFile string
+
 	Description string
 	GitCommit   string
 	Name        string
@@ -31,6 +42,12 @@ func DefaultConfig() Config {
 		Logger: nil,
 
 		// Settings.
+		KubernetesAddress:    "",
+		KubernetesInCluster:  false,
+		KubernetesTLSCaFile:  "",
+		KubernetesTLSCrtFile: "",
+		KubernetesTLSKeyFile: "",
+
 		Description: "",
 		GitCommit:   "",
 		Name:        "",
@@ -42,13 +59,68 @@ func DefaultConfig() Config {
 func New(config Config) (*Command, error) {
 	var err error
 
+	var kubernetesClient *kubernetes.Clientset
+	{
+		var restConfig *rest.Config
+
+		if config.KubernetesInCluster {
+			config.Logger.Log("debug", "creating in-cluster config")
+			restConfig, err = rest.InClusterConfig()
+			if err != nil {
+				return nil, microerror.MaskAny(err)
+			}
+
+			if config.KubernetesAddress != "" {
+				config.Logger.Log("debug", "using explicit api server")
+				restConfig.Host = config.KubernetesAddress
+			}
+		} else {
+			if config.KubernetesAddress == "" {
+				return nil, microerror.MaskAnyf(invalidConfigError, "kubernetes address must not be empty")
+			}
+
+			config.Logger.Log("debug", "creating out-cluster config")
+
+			// Kubernetes listen URL.
+			u, err := url.Parse(config.KubernetesAddress)
+			if err != nil {
+				return nil, microerror.MaskAny(err)
+			}
+
+			restConfig = &rest.Config{
+				Host: u.String(),
+				TLSClientConfig: rest.TLSClientConfig{
+					CAFile:   config.KubernetesTLSCaFile,
+					CertFile: config.KubernetesTLSCrtFile,
+					KeyFile:  config.KubernetesTLSKeyFile,
+				},
+			}
+		}
+
+		kubernetesClient, err = kubernetes.NewForConfig(restConfig)
+		if err != nil {
+			return nil, microerror.MaskAny(err)
+		}
+	}
+	var newUpdater *updater.Updater
+	{
+		updaterConfig := updater.DefaultConfig()
+		updaterConfig.KubernetesClient = kubernetesClient
+		updaterConfig.Logger = config.Logger
+		newUpdater, err = updater.New(updaterConfig)
+		if err != nil {
+			return nil, microerror.MaskAny(err)
+		}
+	}
+
 	var updateCommand *update.Command
 	{
 		updateConfig := update.DefaultConfig()
 		updateConfig.Logger = config.Logger
+		updateConfig.Updater = newUpdater
 		updateCommand, err = update.New(updateConfig)
 		if err != nil {
-			panic(err)
+			return nil, microerror.MaskAny(err)
 		}
 	}
 
--- command/update/command.go
+++ command/update/command.go
@@ -12,6 +12,7 @@ import (
 	"github.com/giantswarm/k8s-endpoint-updater/command/update/flag"
 	"github.com/giantswarm/k8s-endpoint-updater/service/provider"
 	"github.com/giantswarm/k8s-endpoint-updater/service/provider/env"
+	"github.com/giantswarm/k8s-endpoint-updater/service/updater"
 )
 
 var (
@@ -21,7 +22,8 @@ var (
 // Config represents the configuration used to create a new update command.
 type Config struct {
 	// Dependencies.
-	Logger micrologger.Logger
+	Logger  micrologger.Logger
+	Updater *updater.Updater
 }
 
 // DefaultConfig provides a default configuration to create a new update
@@ -29,7 +31,8 @@ type Config struct {
 func DefaultConfig() Config {
 	return Config{
 		// Dependencies.
-		Logger: nil,
+		Logger:  nil,
+		Updater: nil,
 	}
 }
 
@@ -39,10 +42,14 @@ func New(config Config) (*Command, error) {
 	if config.Logger == nil {
 		return nil, microerror.MaskAnyf(invalidConfigError, "logger must not be empty")
 	}
+	if config.Updater == nil {
+		return nil, microerror.MaskAnyf(invalidConfigError, "updater must not be empty")
+	}
 
 	newCommand := &Command{
 		// Dependencies.
-		logger: config.Logger,
+		logger:  config.Logger,
+		updater: config.Updater,
 
 		// Internals.
 		cobraCommand: nil,
@@ -55,16 +62,24 @@ func New(config Config) (*Command, error) {
 		Run:   newCommand.Execute,
 	}
 
-	newCommand.cobraCommand.PersistentFlags().StringSliceVar(&f.Pod.Names, "pod.names", nil, "List of pod names used to lookup pod IPs.")
+	newCommand.CobraCommand().PersistentFlags().StringVar(&f.Kubernetes.Address, "service.kubernetes.address", "http://127.0.0.1:6443", "Address used to connect to Kubernetes. When empty in-cluster config is created.")
+	newCommand.CobraCommand().PersistentFlags().BoolVar(&f.Kubernetes.InCluster, "service.kubernetes.inCluster", false, "Whether to use the in-cluster config to authenticate with Kubernetes.")
+	newCommand.CobraCommand().PersistentFlags().StringVar(&f.Kubernetes.TLS.CaFile, "service.kubernetes.tls.caFile", "", "Certificate authority file path to use to authenticate with Kubernetes.")
+	newCommand.CobraCommand().PersistentFlags().StringVar(&f.Kubernetes.TLS.CrtFile, "service.kubernetes.tls.crtFile", "", "Certificate file path to use to authenticate with Kubernetes.")
+	newCommand.CobraCommand().PersistentFlags().StringVar(&f.Kubernetes.TLS.KeyFile, "service.kubernetes.tls.keyFile", "", "Key file path to use to authenticate with Kubernetes.")
+
 	newCommand.cobraCommand.PersistentFlags().StringVar(&f.Provider.Env.Prefix, "provider.env.prefix", "K8S_ENDPOINT_UPDATER_POD_", "Prefix of environment variables providing pod names.")
 	newCommand.cobraCommand.PersistentFlags().StringVar(&f.Provider.Kind, "provider.kind", "env", "Provider used to lookup pod IPs.")
+
+	newCommand.cobraCommand.PersistentFlags().StringSliceVar(&f.Updater.Pod.Names, "updater.pod.names", nil, "List of pod names used to lookup pod IPs.")
 
 	return newCommand, nil
 }
 
 type Command struct {
 	// Dependencies.
-	logger micrologger.Logger
+	logger  micrologger.Logger
+	updater *updater.Updater
 
 	// Internals.
 	cobraCommand *cobra.Command
@@ -74,7 +89,6 @@ func (c *Command) CobraCommand() *cobra.Command {
 	return c.cobraCommand
 }
 
-// TODO
 func (c *Command) Execute(cmd *cobra.Command, args []string) {
 	c.logger.Log("info", "start updating Kubernetes endpoint")
 
@@ -96,6 +110,8 @@ func (c *Command) Execute(cmd *cobra.Command, args []string) {
 func (c *Command) execute() error {
 	var err error
 
+	// At first we have to sort out which provider to use. This is based on the
+	// flags given to the updater.
 	var newProvider provider.Provider
 	{
 		k := f.Provider.Kind
@@ -103,7 +119,7 @@ func (c *Command) execute() error {
 		case env.Kind:
 			envConfig := env.DefaultConfig()
 			envConfig.Logger = c.logger
-			envConfig.PodNames = f.Pod.Names
+			envConfig.PodNames = f.Updater.Pod.Names
 			envConfig.Prefix = f.Provider.Env.Prefix
 			newProvider, err = env.New(envConfig)
 			if err != nil {
@@ -112,23 +128,25 @@ func (c *Command) execute() error {
 		}
 	}
 
+	// Once we know which provider to use we execute it to lookup the pod
+	// information we are interested in.
 	var podInfos []provider.PodInfo
 	{
+		// TODO provide pod names to Lookup
 		podInfos, err = newProvider.Lookup()
 		if err != nil {
 			return microerror.MaskAny(err)
 		}
 	}
 
-	fmt.Printf("%#v\n", podInfos)
-
-	// TODO
-	//{
-	//	err = c.updater.Update(podInfos)
-	//	if err != nil {
-	//		return microerror.MaskAny(err)
-	//	}
-	//}
+	// Use the updater to actually update the endpoints identified by the provided
+	// flags.
+	{
+		err = c.updater.Update(podInfos)
+		if err != nil {
+			return microerror.MaskAny(err)
+		}
+	}
 
 	return nil
 }
